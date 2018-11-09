772fc25fecba44b1ad21999c8d8f60e59bc2bbc7
Albina Zakirova <zak1804@gmail.com>
2018-11-09 23:49:48 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(katz_deli)
     katz_deli.each_with do |name, index|
       numbered_line << "#{index +1}. name"
     end
-    numbered_line
+    print "The line is currently: #{numbered_line}"
+numbered_line
   else
     'The line is currently empty.'
   end
