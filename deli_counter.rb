565ae51f747862b433eea7456376909554845b4c
Albina Zakirova <zak1804@gmail.com>
2018-11-09 23:47:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size !==0
+    numbered_line = []
+    katz_deli.each_with do |name, index|
+      numbered_line << "#{index +1}. name"
+    end
+    numbered_line
+  else
+    'The line is currently empty.'
+  end
+end
