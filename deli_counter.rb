ee49b8c83a9ee1ae4fa641bef963e06db370f192
Albina Zakirova <zak1804@gmail.com>
2018-11-10 00:25:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,16 @@ def line(deli)
     puts 'The line is currently empty.'
   else
     numbered_line = []
+    
     deli.each_with_index do |name, index|
-      numbered_line << "#{index +1}. #{name}"
+      numbered_line << "#{index}. #{name}"
     end
-    puts "The line is currently: #{numbered_line}"
+    puts "The line is currently: #{numbered_line.join(" ")}"
   end
 end
 
 def take_a_number(deli, name)
-  new_deli_-waiting_line << deli.push(name)
-  puts "Welcome, #{name}. You are number #{deli.size+1} in line deli.size"
+  deli << deli.push(name)
+end_of_line = deli.size
+  puts "Welcome, #{name}. You are number #{end_of_line+1} in line deli.size"
 end
